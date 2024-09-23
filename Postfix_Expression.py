800617583d1d274ba12b0553cf59f3854d0e0c14
Mukul Badge <badgemukul36@gmail.com>
2024-09-23 21:46:19 +0530

    Add files via upload

--- Postfix_Expression.py
+++ Postfix_Expression.py
@@ -1,35 +1,37 @@
+#  Postfix Expression Evaluator
+stack = []
+operators = {'+','-','*','/'}
 
-def postfix_evaluate(expression):
-    # creat stack using list
-    stack = []
-    operators = {'+', '-', '*', '/'}
+#  Read postfix expression from user
+expression = input("Enter Expression: ")
 
+# Operations on stack
+def operations(tokan, operation1, operation2):
+    if tokan == '+':
+        result = operation1 + operation2
+    elif tokan == '-':
+        result = operation1 - operation2
+    elif tokan == '*':
+        result = operation1 * operation2
+    elif tokan == '/':
+        result = operation1 / operation2
+    stack.append(result)
+
+# #  Postfix Expression Evaluator using stack
+def postfix_evaluator(expression):
     tokens = expression.split()
 
     for token in tokens:
         if token in operators:
-            # Pop two operands from the stack
-            operand2 = stack.pop()
-            operand1 = stack.pop()
-            # Perform the operation
-            if token == '+':
-                result = operand1 + operand2
-            elif token == '-':
-                result = operand1 - operand2
-            elif token == '*':
-                result = operand1 * operand2
-            elif token == '/':
-                result = operand1 / operand2
-            # Push the result back onto the stack
-            stack.append(result)
+            # Push operands, pop for operators
+            operation2 = stack.pop()
+            operation1 = stack.pop()
+            # function call of operations
+            operations(token,operation1, operation2)
         else:
-            # Token is an operand, convert it to an integer and push it onto the stack
             stack.append(int(token))
-
-    # The final result is the only element left on the stack
     return stack[0]
 
-expression = input("Enter Expression: ")
-result = postfix_evaluate(expression)
-
+result = postfix_evaluator(expression)
+# Display the evaluation result
 print("Evaluation Result: ",result)
